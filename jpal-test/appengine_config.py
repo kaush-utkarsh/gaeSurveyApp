b6701a0fc5416ead62d61545acd65dea66f5ff13
Utkarsh <utkarsh.kaushik@innovaccer.com>
2014-10-29 16:52:25 +0530

    all clear

--- jpal-test/appengine_config.py
+++
@@ -1,9 +0,0 @@
-from gaesessions import SessionMiddleware
-import os
-COOKIE_KEY = 'dclhdsfodclhdsfodclhdsfodclhdsfodclhdsfo'
-
-def webapp_add_wsgi_middleware(app):
-  from google.appengine.ext.appstats import recording
-  app = SessionMiddleware(app, cookie_key=COOKIE_KEY)
-  app = recording.appstats_wsgi_middleware(app)
-  return app
