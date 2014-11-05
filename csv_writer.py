8c9f2837ea8792decbba4a4db2699ad34a7caf1d
Utkarsh <utkarsh.kaushik@innovaccer.com>
2014-11-05 19:50:21 +0530

    Final Version 1.0

---
+++ csv_writer.py
@@ -0,0 +1,19 @@
+from StringIO import StringIO
+import csv
+
+def getCsv(headers, results):
+    # Header
+    
+    # CSV Stream
+    csvstream = StringIO()
+    writer = csv.writer(csvstream, dialect='excel',quoting=csv.QUOTE_NONNUMERIC)
+    
+    # Data Rows
+    writer.writerow(headers)
+    for row in results:
+        line = [variable.encode("utf-8") for variable in row]
+        writer.writerow(line)
+    
+    contents = csvstream.getvalue()
+    csvstream.close()    
+    return contents
